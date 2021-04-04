baad172afe781566be0161e921f54f10503cb620
ignacio-agullo <81886194+ignacio-agullo@users.noreply.github.com>
2021-04-04 04:50:01 +0200

    Create ip_address.php

---
+++ ip_address.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Enderezo IP</title>
+  </head>
+  <body>
+<?php
+// script that returns the IP address with any HTML format
+echo $_SERVER["REMOTE_ADDR"];
+?>
+  </body>
+</html>
